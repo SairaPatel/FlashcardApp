52f3322ab7ca34478f475c6066dca3aa2997ac6e
Saira Patel <sairapatel1@outlook.com>
2024-08-05 23:43:43 +0100

    Created UI for Edit Card page. still need to resizing bug so that all forms are always visible no matter size of window.

--- FlashCardApp/src/EditCardPage.java
+++ FlashCardApp/src/EditCardPage.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.*;
 
@@ -7,34 +9,118 @@ public class EditCardPage extends JFrame{
     JFrame f;
 
     EditCardPage(){
+
+        setLayout(new GridBagLayout());
+
+        // title label
+        JLabel titleLabel = new JLabel("Title:");
+
+        GridBagConstraints titleLabelGBC = UIController.getGBC(0, 0);
+        add(titleLabel, titleLabelGBC);
+        
+        // title input
+        JTextField titleInput = new JTextField();        
+
+        GridBagConstraints titleInputGBC = UIController.getGBC(0, 1);
+        add(titleInput, titleInputGBC);
+
+
+        // set label
+        JLabel setLabel = new JLabel("Set:");
+
+        GridBagConstraints setLabelGBC = UIController.getGBC(0, 2);
+        add(setLabel, setLabelGBC);
+
+        // set input
+        JComboBox<String> setInput = new JComboBox<String>();
+        setInput.setEditable(true);
+
+        GridBagConstraints setInputGBC = UIController.getGBC(0, 3);
+        add(setInput, setInputGBC);
         
         
-        // Add-tag textfield and button
-        JTextField tagTextField = new JTextField(10); 
-        JButton addTagBtn = new JButton("Add Tag");
-        JPanel addTagPanel = new JPanel();
-        addTagPanel.setLayout(new BoxLayout(addTagPanel, BoxLayout.X_AXIS));
-        addTagPanel.add(tagTextField);
-        addTagPanel.add(addTagBtn);
+        // tags label
+        JLabel tagsLabel = new JLabel("Tags:");
         
-        // scrollable tags list
-        String[] tags = {"Storage", "Secondary Storage", "HDD", "Memory", "HDD", "Memory", "HDD", "Memory"};
-        JList<String> tagList = new JList<String>(tags);
-        JScrollPane tagListPane = new JScrollPane(tagList);
+        GridBagConstraints tagsLabelGBC = UIController.getGBC(0, 4);
+        add(tagsLabel, tagsLabelGBC);
+
+
+        // tags list
+        String[] tags = {"Storage", "Secondary Storage and Primary Storage", "HDD", "Memory", "HDD", "Memory", "HDD", "Memory"};
+        JList<String> tagsList = new JList<String>(tags);
+        JScrollPane tagsListPane = new JScrollPane(tagsList);
+        tagsListPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        GridBagConstraints tagsGBC = UIController.getGBC(0, 5, 0, 1);
+        tagsGBC.fill = GridBagConstraints.BOTH;
+        add(tagsListPane, tagsGBC);
         
-        // Tags Panel
-        JPanel tagsPanel = new JPanel();
+        // add tag label
+        JLabel addLabel = new JLabel("Add Tag:");
+
+        GridBagConstraints addLabelGBC = UIController.getGBC(0, 6 );
+        add(addLabel, addLabelGBC);
+
+        // add input
+        JComboBox<String> addInput = new JComboBox<String>(tags);
+        addInput.setEditable(true);
+
+        GridBagConstraints addInputGBC = UIController.getGBC(0, 7);
+        add(addInput, addInputGBC);
+
+
+        // add button
+        JButton addButton = new JButton("Add");
+
+        GridBagConstraints addButtonGBC = UIController.getGBC(0, 8);
+        add(addButton, addButtonGBC);
+
+
+        // Cards Frame ----------------
+        JPanel cardsPanel = new JPanel(new GridBagLayout());
+
+
+
+        // front label
+        JLabel frontLabel = new JLabel("Front:");
+
+        GridBagConstraints frontLabelGBC = UIController.getGBC(0, 0);
+        cardsPanel.add(frontLabel, frontLabelGBC);
         
-        tagsPanel.setLayout(new BoxLayout(tagsPanel, BoxLayout.Y_AXIS));
-        tagsPanel.add(addTagPanel);
-        tagsPanel.add(tagListPane);
-        tagsPanel.setSize(new Dimension(100,100));
-        //tagsPanel.setBounds(10,10, 300,200);
-        add(tagsPanel, BorderLayout.CENTER);
+        // front input
+        JTextArea frontInput = new JTextArea();      
+        JScrollPane frontPane = new JScrollPane(frontInput);  
+
+        GridBagConstraints frontInputGBC = UIController.getGBC(0, 1, 1, 1);
+        frontInputGBC.fill = GridBagConstraints.BOTH;
+        cardsPanel.add(frontPane, frontInputGBC);
+
+        // back label
+        JLabel backLabel = new JLabel("Back:");
+
+        GridBagConstraints backLabelGBC = UIController.getGBC(0, 2);
+        cardsPanel.add(backLabel, backLabelGBC);
+        
+        // back input
+        JTextArea backInput = new JTextArea();        
+        JScrollPane backPane = new JScrollPane(backInput);
+
+        GridBagConstraints backInputGBC = UIController.getGBC(0, 3, 1, 1);
+        backInputGBC.fill = GridBagConstraints.BOTH;
+        cardsPanel.add(backPane, backInputGBC);
+
+
+
+        // cards panel gbc
+        GridBagConstraints cardsPanelGBC = UIController.getGBC(1,0,1,1);
+        cardsPanelGBC.fill = GridBagConstraints.BOTH;
+        cardsPanelGBC.gridheight = 9;
+        add(cardsPanel, cardsPanelGBC);
+
 
         // set layout
         setSize(400, 400);
-        setLayout(null);
         setVisible(true);
 
     }
---
+++ FlashCardApp/src/UIController.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+
+public class UIController {
+
+    final static Color darkColour = new Color(220, 40, 80);
+    final static Color midColour = new Color(245, 245, 245);
+    final static Color lightColour = new Color(255, 255, 255);
+
+   
+    /**
+     * Returns a GridBagConstraints object with: ipadx = 2 ipady = 2, weightx and weighty = 0.
+     * And gridx, gridy, values specified by params
+     *  
+     * @param gridx the column of the component
+     * @param gridy the row of the component
+     * @return the GridBagConstraints object with certain values set
+     */
+    static public GridBagConstraints getGBC(int gridx, int gridy){
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.ipadx = 3;
+        gbc.ipady = 3;
+        gbc.insets = new Insets(3,3,3,3);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        return gbc;
+    }
+
+    /**
+     * Returns a GridBagConstraints object with: ipadx = 2 ipady = 2. 
+     * And gridx, gridy, weightx and weighty values specified by params
+     *  
+     * @param gridx the column of the component
+     * @param gridy the row of the component
+     * @param weightx the weight of the component's column
+     * @param weighty the weight of the component's row
+     * @return the GridBagConstraints object with certain values set
+     */
+    static public GridBagConstraints getGBC(int gridx, int gridy, double weightx, double weighty){
+        GridBagConstraints gbc = getGBC(gridx, gridy);
+        gbc.weightx = weightx;
+        gbc.weighty = weighty;
+
+        return gbc;
+    }
+
+    static public Font getFont(int fontsize){
+        return new Font("sans serif", Font.BOLD, fontsize);
+    }
+}
